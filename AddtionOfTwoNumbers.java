50e0ecaed1bafe4fe92ab74fb7b8aee4ac3ceda0
REVANSIDDA <patilrevan1618@gmail.com>
2025-09-20 23:42:01 +0530

    AddtionOfTwoNumbers.java
    Used Concatination

---
+++ AddtionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class AdditionOfTwoNumbers
+{
+  public static void main(String [] args)
+  {
+    Scanner s = new Scanner(System.in);
+     System.out.println("Enter Two Numbers");
+    int a = s.nextInt(), b=s.nextInt();
+     int res = a+b;
+    System.out.println( a + "+" + b + "=" + res );
+  }
+}
+     
