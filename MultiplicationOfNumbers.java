dd3f9f4e07853cf516e56cbb13ac5374715d6375
REVANSIDDA <patilrevan1618@gmail.com>
2025-09-20 23:49:53 +0530

    MultiplicationOfNumbers.java
    Used Concatination

---
+++ MultiplicationOfNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class MultiplicationOfTwoNumbers
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter Two Numbers");
+		
+		int a =s.nextInt(), b=s.nextInt();
+		
+		int res = a*b;
+		
+		System.out.println(a+ "*" + b + "=" + res);
+	}
+}
