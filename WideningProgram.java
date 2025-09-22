42a2e77f7c8f8021138614de4921f0b7efcecb3a
REVANSIDDA <patilrevan1618@gmail.com>
2025-09-22 20:06:32 +0530

    WideningProgram.java

---
+++ WideningProgram.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Widening 
+{
+  public static void main (String [] args)
+  {
+    Scanner s = new Scanner(System.in);
+    System.out.println("enter a number");
+    
+    int i = s.nextInt();
+    
+    long l = i;
+    float f = i;
+    double d = i;
+    System.out.println(i);
+    System.out.println(l);
+    System.out.println(f);
+    System.out.println(d);
+    
+  }
+}
+    
