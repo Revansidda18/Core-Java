4a542280ec1ac9e10366245e6cde2f8df209f1cf
REVANSIDDA <patilrevan1618@gmail.com>
2025-09-30 23:09:34 +0530

    FirstDigitOfNumber.java

---
+++ FirstDigitOfNumber.java
@@ -0,0 +1,21 @@
+Example => input - 765, output - 7.
+
+
+import java.util.Scanner;
+class FirstDigit{
+  public static void main(String [] args){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter a Number");
+    int n=s.nextInt();
+    if(n<0)
+    {
+      n=-n;
+    }
+    while(n>=0)
+    {
+      n/=10;
+    }
+    System.out.println("first digit of " + n);
+  }
+}
+
